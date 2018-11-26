a47ef1b3503763acb8b83fd21110f339a9c830ce
fernandodmaqueda <fernandomaqueda@live.com>
2018-11-25 22:01:11 -0300

    4. Vamos a añadir una nueva característica a nuestra aplicación. Para eso, vamos a crear una nueva rama con nombre addBye 5. En la nueva rama creada vamos a añadir el fichero bye.html en src/main/resources/templates con el contenido indicado en el punto 6. 6 . (Contenido de bye.html) 7. y añadimos a la clase GreetingController el método indicado. 8. Hacer un commit de estos cambios.
    Correción:
    9. Corregir el último commit para que el defaultValue sea "Universe" en lugar de "World"

--- java-hello/src/main/java/hello/GreetingController.java
+++ java-hello/src/main/java/hello/GreetingController.java
@@ -14,4 +14,9 @@ public class GreetingController {
         return "greeting";
     }
 
+	@RequestMapping("/bye")
+	public String bye(@RequestParam(value="name", required=false, defaultValue="Universe") String name, Model model) {
+		model.addAttribute("name", name);
+		return "bye";
+	}
 }
